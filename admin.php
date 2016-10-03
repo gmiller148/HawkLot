fd25de3c901faa7d55e56fc1df31cb2bee36051c
G-Money242 <grant242@gmail.com>
2016-10-03 12:44:31 -0500

    Add files via upload

--- admin.php
+++ admin.php
@@ -1,15 +1,21 @@
-<?php
-
-#session_start();
-
-if(!$_SESSION['logon']){
-#  header("Location:index.php");
+<?php if($_SESSION['logon']!=4) {
+  session_start();
+  header("refresh:3; url=index.php");
+  echo "PLEASE LOG ON."."\n";
+  echo $_SESSION['logon']. "\n";
+  echo session_id();
+  echo $_SESSION['username']. "\n";
+  exit;
 }
-
 ?>
+
 
 <html>
 <body>
-<p>fgfds</p>
+  <p>Redirecting you in 3 seconds...</p>
+<?php if($_SESSION['logon']==4) : ?>
+  <p>This is a hidden element</p>
+  <?php echo $_SESSION['username']; ?>
+<?php endif; ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta charset="utf-8" />
     <title>HawkLot</title>
 </head>
-<body>
+<body style="background-color:cyan;">
     <h1>HawkLot</h1>
     <p>A parking-space sharing service.</p>
     <p>This year, the parking lot rules at Maine South changed so that specific spots are reserved for the entire year. But, nobody users his/her spot every day--there are days when a substantial portion of the parking lot sits empty. This is an inefficient system, and an unfair system for those who were not able to get a parking pass.</p>
@@ -47,11 +46,9 @@
         if (!$conn) {
           die("Connection failed: " . mysqli_connect_error());
         }
-
         mysqli_select_db($conn, $db);
 
         if(isset($_POST['login'])) {
-
 
           $username = $_POST['user'];
           $pass = $_POST['pass'];
@@ -59,49 +56,26 @@
           $run_user = mysqli_query($conn, $sel_user);
           $check_user = mysqli_num_rows($run_user);
 
-      #    printf("Result set has %d rows.\n", $check_user);
-
           if($check_user > 0) {
             $row = mysqli_fetch_assoc($run_user);
             $pw = $row['pass'];
-            
+
             if(password_verify($pass, $pw)) {
-
               session_start();
-
               $_SESSION['username'] = $username;
-        #    echo "eyeyeyeyeye";
-        #    echo session_id();
-              #header("Location: admin.php");
-            #if(isset($username) && isset($password)){
-        #      echo 'wassaas';
-              $_SESSION['logon'] = true;
-        #      echo $_SESSION['logon'];
-              #header('location: admin.php');
-          #  }
-        #    echo 'USER FOUND';
-
-          }
-          else {
+              $_SESSION['logon'] = 4;
+            }else {
             echo "<script>alert('Email or password is not correct, try again')</script>";
+            }
           }
         }
-
-
-        }
-
-
-
-        /*$sql = "INSERT INTO users (user_id,user_password) VALUES ('$user','$password')";
-        if (mysqli_query($conn, $sql)) {
-          echo "it worked";
-        } else {
-          echo "did not work. error: " . mysqli_error($conn);
-        }
-        */
         mysqli_close($conn);
         ?>
     </form>
+    <?php if($_SESSION['logon']) : ?>
+      <?php echo session_id(); ?>
+      <meta http-equiv="refresh" content="2;url=admin.php">
+    <?php endif; ?>
 
 </body>
 </html>
