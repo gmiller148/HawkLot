6bcab622a33aa17f9499977df57f9cbc38f89ee3
G-Money242 <grant242@gmail.com>
2016-10-07 22:21:39 -0500

    Add files via upload

--- register.php
+++ register.php
@@ -23,10 +23,17 @@
         <td><input type="password" name="pass_check" required="required"></td>
       </tr>
       <tr>
+        <td colspan="3" align="left">
+          <input type="radio" name="access" value="renter" checked> I want to rent parking spots.<br>
+          <input type="radio" name="access" value="owner"> I own a parking spot. <br>
+        </td>
+      </tr>
+      <tr>
         <td colspan="3" align="center">
           <input type="submit" name="register" value="Register">
         </td>
       </tr>
+
     </table>
 
     <?
@@ -44,22 +51,31 @@
 
       $username = mysqli_real_escape_string($conn,$_POST['user']);
       $pass = mysqli_real_escape_string($conn,$_POST['pass']);
-
       $pass_check = $_POST['pass_check'];
+
       if($pass != $pass_check) {
         echo "<script>alert('Passwords are not the same')</script>";
         mysqli_close($conn);
         exit;
       }
+
       $hashAndSalt = password_hash($pass, PASSWORD_BCRYPT);
 
       $sel_user = "SELECT * FROM users WHERE username='$username'";
       $run_user = mysqli_query($conn, $sel_user);
 
       $check_user = mysqli_num_rows($run_user);
+      $level_of_access = $_POST['access'];
+      $priv = -1;
+      if($level_of_access == 'renter') {
+        $priv = 1;
+      } else if($level_of_access == 'owner') {
+        $priv = 2;
+      }
 
+      //1 = renter :: 2 = owner :: 3 = admin
       if($check_user == 0) {
-        $new_user = "INSERT INTO users(username, pass) VALUES('$username', '$hashAndSalt')";
+        $new_user = "INSERT INTO users(username, pass, privelege) VALUES('$username', '$hashAndSalt', '$priv')";
         $create_user = mysqli_query($conn, $new_user);
         echo "<script>alert('User was successfully created')</script>";
         mysqli_close($conn);
