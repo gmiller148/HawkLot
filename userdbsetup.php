7599ae34ff9eda78d05d2681e1fda1cb5b55f3e8
G-Money242 <grant242@gmail.com>
2016-10-07 21:32:20 -0500

    Add files via upload

---
+++ userdbsetup.php
@@ -0,0 +1,27 @@
+<?php
+
+$db = 'UserDB';
+$conn = mysqli_connect("localhost:8889", "root", "root");
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+mysqli_select_db($conn, $db);
+
+$createTable = "CREATE TABLE IF NOT EXISTS users (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   username text NOT NULL,
+   pass binary(60) NOT NULL,
+   privelege tinyint(2) NOT NULL
+ ) ENGINE = MYISAM";
+$run_createTable = mysqli_query($conn, $createTable);
+$alterTable0 = "ALTER TABLE users MODIFY COLUMN id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY";
+$alterTable1 = "ALTER TABLE users MODIFY COLUMN username text NOT NULL";
+$alterTable2 = "ALTER TABLE users MODIFY COLUMN pass text NOT NULL";
+$alterTable3 = "ALTER TABLE users MODIFY COLUMN privelege tinyint(2) NOT NULL";
+$run_alterTable0 = mysqli_query($conn, $alterTable0);
+$run_alterTable1 = mysqli_query($conn, $alterTable1);
+$run_alterTable2 = mysqli_query($conn, $alterTable2);
+$run_alterTable3 = mysqli_query($conn, $alterTable3);
+
+?>
