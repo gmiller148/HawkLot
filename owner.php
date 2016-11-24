050983f0a4e1fb04078f75e5ced0f8716604f136
G-Money242 <grant242@gmail.com>
2016-11-23 23:09:19 -0600

    Add files via upload

--- owner.php
+++ owner.php
@@ -14,5 +14,12 @@
       echo "Hello renter: ".$_SESSION['username'];
     }
     ?>
+
+        <div class="container">
+          <div class="row main">
+            <h1><a href="register-spot.php">Need to register a spot?</a></h1>
+          </div>
+        </div>
+
 </html>
 </body>
